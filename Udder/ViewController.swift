3c46fdbb3efb64f384429f13994e8bf7c759e225
Mohammed Qasedo <mohammed.qasedo@exalt.ps>
2022-07-24 12:41:21 +0300

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Mohammed Qasedo on 24/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
