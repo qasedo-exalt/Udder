2b7d996844ce8505f80e610f00afd057a400233c
Mohammed Qasedo <mohammed.qasedo@exalt.ps>
2022-07-24 13:31:13 +0300

    override view will disappear method

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -17,6 +17,10 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         print("Hello, World!!")
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("View disappear")
+    }
 
 }
 
